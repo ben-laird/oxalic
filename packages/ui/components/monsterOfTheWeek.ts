31fb5891f6139e62fec76d1213a1090fb3744bad
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-02-02 16:27:18 -0500

    adapt to rustic enum api in example

--- packages/ui/components/monsterOfTheWeek.ts
+++ packages/ui/components/monsterOfTheWeek.ts
@@ -1,26 +1,23 @@
-import { Creator, RusticEnum, V, Variant } from "rustic-enum";
+import { RusticEnum, V, Variant } from "rustic-enum";
 
-interface RollDetails {
+export interface RollDetails {
   roll: number;
   actions: string[];
 }
 
-type RollActionV = V.Interface<{
-  Failure: RollDetails;
-  MixedSuccess: RollDetails;
-  Success: RollDetails;
-}>;
-
-class RollAction<
-  V extends V.Ext<RollActionV> = V.Ext<RollActionV>
-> extends RusticEnum<RollActionV, V> {
-  static new: Creator.Standard<RollAction> = (type, value) =>
-    new RollAction({ type, value });
+enum RollActionE {
+  Failure,
+  MixedSuccess,
+  Success,
 }
+
+type RollActionV = V.Enum<keyof typeof RollActionE, RollDetails>;
+
+class RollAction extends RusticEnum<RollActionV> {}
 
 const rawRoll = (sides: number) => Math.floor(Math.random() * (sides + 1));
 
-export const rollDice = (): RollAction => {
+export const rollDice = () => {
   const roll = rawRoll(6) + rawRoll(6);
 
   return roll < 7
@@ -28,19 +25,19 @@ export const rollDice = (): RollAction => {
         type: "Failure",
         value: new Variant({
           roll,
-          actions: ["Mark experience", "The GM will take a hard move"]
-        })
+          actions: ["Mark experience", "The GM will take a hard move"],
+        }),
       })
     : roll < 10
     ? new RollAction({
         type: "MixedSuccess",
-        value: new Variant({ roll, actions: ["Make a hard choice"] })
+        value: new Variant({ roll, actions: ["Make a hard choice"] }),
       })
     : new RollAction({
         type: "Success",
         value: new Variant({
           roll,
-          actions: ["Accomplish what you set out to do"]
-        })
+          actions: ["Accomplish what you set out to do"],
+        }),
       });
 };

6e1a0ecd081b35f6bafb7d8e6106250f11c51075
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-02-27 16:06:58 -0500

    begin renaming

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-typelaunch-turbo`
-  extends: ["typelaunch-turbo"],
+  extends: ["@oxalic/oxalic"],
 };
